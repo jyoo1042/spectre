1bddacd598a034ca08a6ff31f52f65e0dd2afdc1
Jooheon Yoo <jy884@cornell.edu>
2025-01-30 12:52:11 -0500

    Add StressEnergyTensor function

--- tests/Unit/PointwiseFunctions/Hydro/Test_StressEnergy.cpp
+++ tests/Unit/PointwiseFunctions/Hydro/Test_StressEnergy.cpp
@@ -17,17 +17,20 @@ namespace hydro {
 SPECTRE_TEST_CASE("Unit.PointwiseFunctions.Hydro.StressEnergy",
                   "[Unit][Hydro]") {
   pypp::SetupLocalPythonEnvironment local_python_env(
-      "PointwiseFunctions/Hydro/");
-  const DataVector used_for_size(5);
+      "PointwiseFunctions/Hydro/Python/");
+  const DataVector used_for_size(1);
   pypp::check_with_random_values<1>(&energy_density<DataVector>,
-                                    "TestFunctions", {"energy_density"},
+                                    "Test_StressEnergy", {"energy_density"},
                                     {{{0.0, 1.0}}}, used_for_size);
   pypp::check_with_random_values<1>(&momentum_density<DataVector>,
-                                    "TestFunctions", {"momentum_density"},
+                                    "Test_StressEnergy", {"momentum_density"},
                                     {{{0.0, 1.0}}}, used_for_size);
-  pypp::check_with_random_values<1>(&stress_trace<DataVector>, "TestFunctions",
-                                    {"stress_trace"}, {{{0.0, 1.0}}},
-                                    used_for_size);
+  pypp::check_with_random_values<1>(&stress_trace<DataVector>,
+                                    "Test_StressEnergy", {"stress_trace"},
+                                    {{{0.0, 1.0}}}, used_for_size);
+  pypp::check_with_random_values<1>(
+      &stress_energy_tensor<DataVector>, "Test_StressEnergy",
+      {"stress_energy_tensor"}, {{{0.0, 1.0}}}, used_for_size);
 }
 
 }  // namespace hydro
