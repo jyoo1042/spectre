82ded3a1d0eed385de0321e227c2210f282afc5f
Jooheon Yoo <jy884@cornell.edu>
2025-01-30 12:51:42 -0500

    Add ComovingMagneticField function and its test

--- tests/Unit/PointwiseFunctions/Hydro/Test_ComovingMagneticField.cpp
+++ tests/Unit/PointwiseFunctions/Hydro/Test_ComovingMagneticField.cpp
@@ -21,6 +21,14 @@ SPECTRE_TEST_CASE("Unit.PointwiseFunctions.Hydro.ComovingMagneticField",
       "PointwiseFunctions/Hydro/");
   const DataVector used_for_size(5);
   pypp::check_with_random_values<1>(
+      static_cast<tnsr::A<DataVector, 3> (*)(
+          const tnsr::I<DataVector, 3>&, const tnsr::I<DataVector, 3>&,
+          const Scalar<DataVector>&, const Scalar<DataVector>&,
+          const tnsr::I<DataVector, 3>&, const Scalar<DataVector>&)>(
+          &comoving_magnetic_field<DataVector>),
+      "TestFunctions", "comoving_magnetic_field", {{{0.0, 1.0}}},
+      used_for_size);
+  pypp::check_with_random_values<1>(
       static_cast<tnsr::a<DataVector, 3> (*)(
           const tnsr::i<DataVector, 3>&, const tnsr::i<DataVector, 3>&,
           const Scalar<DataVector>&, const Scalar<DataVector>&,
